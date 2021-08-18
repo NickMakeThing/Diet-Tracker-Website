71c73c669ea0f0394ab80436ef50feb6ae212ad8
NickMakeThing <nicksegalwork@gmail.com>
2021-08-18 16:08:41 +1000

    fixed static files

--- diettracker/settings.py
+++ diettracker/settings.py
@@ -1,7 +1,5 @@
 """
 TODO:
-    postgres
-    environment variables
     models
     static stuff?
 """
@@ -16,7 +14,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get("DJANGO_KEY")
+
+SECRET_KEY = os.environ.get("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -121,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
