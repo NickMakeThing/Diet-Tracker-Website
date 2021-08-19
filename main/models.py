9edaf920bded11ac0c3fb523d74304c288435e5f
NickMakeThing <nicksegalwork@gmail.com>
2021-08-19 17:05:54 +1000

    added database tables

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+"""
+note: all nutritional info is grams per 100g
+energy is in kjs
+table product
+name, unique hash, current weight, energy, proteins, carbs, fats, sugars?
 
-# Create your models here.
+table weight_changes
+foreign_key: product, date/time, consumption_amount/weight_change
+"""
+
+class Product(models.Model):
+    name = models.CharField(max_length=50,null=True)
+    current_weight = models.IntegerField()
+    energy = models.IntegerField()
+    protein = models.IntegerField()
+    fat = models.IntegerField()
+    carb = models.IntegerField()
+    sugars = models.IntegerField()
+
+class WeightChanges(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    weight_change = models.IntegerField()
